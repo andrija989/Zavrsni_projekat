4f4ae99742c0ec1196c881c2881827dbb733473b
Andrija Perovic <andrija989@gmail.com>
2019-04-16 16:07:51 +0200

    Kreirana baza podataka

--- posts.php
+++ posts.php
@@ -1,3 +1,20 @@
+<?php
+    
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
+
 <html>
 <div class="blog-post">
                 <h2 class="blog-post-title"><a href = "">Sample blog post</a></h2>
