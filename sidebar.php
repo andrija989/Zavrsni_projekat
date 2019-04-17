7c7f7d4f1bbd6393157a6e0f0e56935ca17ede29
Andrija Perovic <andrija989@gmail.com>
2019-04-17 21:17:17 +0200

    Sitne css prepravke

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
     <aside class="col-sm-3 ml-sm-auto blog-sidebar">
     <div class="sidebar-module sidebar-module-inset">
         <?php
-        $sqlside ="SELECT * FROM posts ORDER BY created_at DESC;";
+        $sqlside ="SELECT * FROM posts ORDER BY created_at DESC LIMIT 5;";
         $statement = $connection->prepare($sqlside);
 
         $statement->execute();
@@ -12,10 +12,14 @@
         $posts = $statement->fetchAll(); 
 
         ?>
+        <h3>Last 5 posts: </h3>
         <?php
+        $i=0;
         foreach ($posts as $post) {
+        $i++
         ?> 
-        <a href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['title']) ?></a>
+        
+        <a class="sidebar-5-links" href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($i . " - ") ?><?php  echo($post['title']) ?></a>
                 
 
         <?php 
